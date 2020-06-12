0f553c12bb7c3cf0940bd0c1eb308f5f073b6b16
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-06-12 17:41:40 +0300

    updates

--- UniversityLib/Models/Teacher.cs
+++ UniversityLib/Models/Teacher.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace UniversityLib.Models
 {
-   public class Teacher
+   public class Teacher:Person
     {
+        public List<string> Subjects { get; private set; }
+        public Teacher(string name, string surname, int age):base(name,surname,age)
+        {
+
+        }
+        public Teacher(string name ,string surname ,int age,string subject): base(name, surname, age)
+        {
+            if (subject!=null)
+            {
+                Subjects
+            }
+            else
+            {
+                throw new NullReferenceException("subject is null");
+            }
+        }
     }
 }
